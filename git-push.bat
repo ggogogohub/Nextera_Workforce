be44137bcef30ed07c4a9ad144491aa1febf9375
mahakaal <mahakaal@MAHAKAAL>
2025-02-21 21:54:16 +0000

    Auto-commit on Fri 02/21/2025 21:54:16.48

--- git-push.bat
+++ git-push.bat
@@ -1,6 +1,9 @@
 @echo off
 set /p desc=Commit message: 
-if "%desc%"=="" set desc=Auto-commit on %date% %time%
+if "%desc%"=="" (
+    set desc=Auto-commit on %date% %time%
+)
+
 git add .
 git commit -m "%desc%"
 git push origin master
--- watcher.bat
+++ watcher.bat
@@ -1,5 +1,6 @@
 @echo off
+setlocal
 :loop
-timeout /t 5
+timeout /t 30 >nul  ⬅ (Runs every 30 seconds)
 call git-push.bat
 goto loop
